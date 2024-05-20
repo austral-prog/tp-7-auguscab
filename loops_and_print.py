0db6625928584a6a22f68cecadc76697dac2e5c0
auguscab <162723093+auguscab@users.noreply.github.com>
2024-05-20 20:50:14 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,14 @@
 def enumerate_list(list):
-    return list
+    result= []
+    for i in list:
+        if i:
+            result.append(f"{len(result)}. {i}")
+    return result
 
 
 def enumerate_backwards(list):
-    return list
+    resultado= []
+    for i in list:
+        if i:
+            resultado.append(f"{len(resultado)}. {i[::-1]}")
+    return resultado
